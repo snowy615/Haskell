b3ea725ef42ec858dd3e00582ec0951d8dfa73a9
snowy615 <yansnow615@gmail.com>
2025-08-28 10:52:46 -0700

    bmiTell

--- learning/src/Types.hs
+++ learning/src/Types.hs
@@ -63,3 +63,11 @@ sum' (x:xs) = x + sum' xs
 capital :: String -> String
 capital "" = "Empty string, whoops!"
 capital all@(x:xs) = "The first letter of " ++ all ++ " is " ++ [x]
+
+bmiTell :: (RealFloat a) => a -> String
+bmiTell bmi
+    | bmi <= 18.5 = "You're underweight, you emo, you!"
+    | bmi <= 25.0 = "You're supposedly normal. Pffft, I bet you're ugly!"
+    | bmi <= 30.0 = "You're fat! Lose some weight, fatty!"
+    | otherwise   = "You're a whale, congratulations!"
+    
