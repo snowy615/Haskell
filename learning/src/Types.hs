f4aed76be9f13f6cc50e8b9d9c4fb8e4a58a8674
snowy615 <yansnow615@gmail.com>
2025-08-28 11:14:30 -0700

    bmi where

--- learning/src/Types.hs
+++ learning/src/Types.hs
@@ -66,10 +66,11 @@ capital all@(x:xs) = "The first letter of " ++ all ++ " is " ++ [x]
 
 bmiTell :: (RealFloat a) => a -> a -> String
 bmiTell weight height
-    | weight / height ^ 2 <= 18.5 = "You're underweight, you emo, you!"
-    | weight / height ^ 2 <= 25.0 = "You're supposedly normal. Pffft, I bet you're ugly!"
-    | weight / height ^ 2 <= 30.0 = "You're fat! Lose some weight, fatty!"
+    | bmi <= 18.5 = "You're underweight, you emo, you!"
+    | bmi <= 25.0 = "You're supposedly normal. Pffft, I bet you're ugly!"
+    | bmi <= 30.0 = "You're fat! Lose some weight, fatty!"
     | otherwise   = "You're a whale, congratulations!"
+    where bmi = weight / height ^ 2
 
 max' :: (Ord a) => a -> a -> a
 max' a b
@@ -85,4 +86,3 @@ a `myCompare` b
     | a > b     = GT
     | a == b    = EQ
     | otherwise = LT
-    
