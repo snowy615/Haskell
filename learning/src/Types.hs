a1ceaa8d1777101bc18c15086f450726ece6fa07
snowy615 <yansnow615@gmail.com>
2025-08-27 20:49:10 -0700

    better implementation

--- learning/src/Types.hs
+++ learning/src/Types.hs
@@ -25,5 +25,9 @@ charName 'a' = "Albert"
 charName 'b' = "Broseph"
 charName 'c' = "Cecil"
 
+-- addVectors :: (Num a) => (a, a) -> (a, a) -> (a, a)
+-- addVectors a b = (fst a + fst b, snd a + snd b)
+
+--better way
 addVectors :: (Num a) => (a, a) -> (a, a) -> (a, a)
-addVectors a b = (fst a + fst b, snd a + snd b)
+addVectors (x1, y1) (x2, y2) = (x1 + x2, y1 + y2)
