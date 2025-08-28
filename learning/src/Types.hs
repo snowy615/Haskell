5586bd1c46e3953351f40a750ed8f18ed8275565
snowy615 <yansnow615@gmail.com>
2025-08-28 10:39:38 -0700

    length

--- learning/src/Types.hs
+++ learning/src/Types.hs
@@ -52,5 +52,7 @@ tell (x:[]) = "The list has one element: " ++ show x
 tell (x:y:[]) = "The list has two elements: " ++ show x ++ " and " ++ show y
 tell (x:y:_) = "This list is long. The first two elements are: " ++ show x ++ " and " ++ show y
 
-
+length' :: (Num b) => [a] -> b
+length' [] = 0
+length' (_:xs) = 1 + length' xs
 
