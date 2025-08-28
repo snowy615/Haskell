96bce48ef21357540bd6773ed953e2a96287b882
snowy615 <yansnow615@gmail.com>
2025-08-27 20:19:52 -0700

    sayme function

--- learning/src/Types.hs
+++ learning/src/Types.hs
@@ -7,3 +7,11 @@ factorial n = product [1..n]
 
 circumference' :: Double -> Double
 circumference' r = 2 * pi * r
+
+sayMe :: (Integral a) => a -> String
+sayMe 1 = "One!"
+sayMe 2 = "Two!"
+sayMe 3 = "Three!"
+sayMe 4 = "Four!"
+sayMe 5 = "Five!"
+sayMe x = "Not between 1 and 5"
