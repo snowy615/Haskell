4b7a5b2e0c9e19d3b048c1e05cde13e89ba54b0d
snowy615 <yansnow615@gmail.com>
2025-08-30 18:20:28 -0700

    filter

--- learning/src/Types.hs
+++ learning/src/Types.hs
@@ -183,6 +183,13 @@ flip' :: (a -> b -> c) -> (b -> a -> c)
 flip' f x y = f y x
 
 
-map :: (a -> b) -> [a] -> [b]
-map _ [] = []
-map f (x:xs) = f x : map f xs
+map' :: (a -> b) -> [a] -> [b]
+map' _ [] = []
+map' f (x:xs) = f x : map' f xs
+
+
+filter' :: (a -> Bool) -> [a] -> [a]
+filter' _ [] = []
+filter' p (x:xs)
+    | p x       = x : filter' p xs
+    | otherwise = filter' p xs
