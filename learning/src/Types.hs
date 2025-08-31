94539cf760ff3c097a45c6b2c7f193ba313f691b
snowy615 <yansnow615@gmail.com>
2025-08-30 17:14:40 -0700

    zipWith

--- learning/src/Types.hs
+++ learning/src/Types.hs
@@ -173,3 +173,8 @@ quicksort (x:xs) =
 
 applyTwice :: (a -> a) -> a -> a
 applyTwice f x = f (f x)
+
+zipWith' :: (a -> b -> c) -> [a] -> [b] -> [c]
+zipWith' _ [] _ = []
+zipWith' _ _ [] = []
+zipWith' f (x:xs) (y:ys) = f x y : zipWith' f xs ys
