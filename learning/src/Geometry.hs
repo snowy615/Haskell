4530a395d577d5696a01761bb186cc03762f81e7
snowy615 <yansnow615@gmail.com>
2025-09-19 16:30:36 -0700

    Geometry

---
+++ learning/src/Geometry.hs
@@ -0,0 +1,18 @@
+module Geometry where
+( sphereVolume, 
+  sphereArea,
+  cubeVolume,
+  cubeArea
+) where
+
+sphereVolume :: Float -> Float
+sphereVolume r = (4.0 / 3.0) * pi * r ^ 3
+
+sphereArea :: Float -> Float
+sphereArea r = 4.0 * pi * r ^ 2
+
+cubeVolume :: Float -> Float
+cubeVolume a = a ^ 3
+
+cubeArea :: Float -> Float
+cubeArea a = 6 * a ^ 2
