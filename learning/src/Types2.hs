7eff6c12298d65d1e3cb7185177569ab81368529
snowy615 <yansnow615@gmail.com>
2025-09-18 21:18:09 -0700

    import

--- learning/src/Types2.hs
+++ learning/src/Types2.hs
@@ -3,6 +3,7 @@ module Types2 where
 import Data.List
 -- import Data.List(nub, sort)       =import only nub and sort
 -- import Data.List hiding (nub)     =import everything except nub
+import qualified Data.Map as Map
 
 numUniques :: (Eq a) => [a] -> Int
 numUniques = length . nub
