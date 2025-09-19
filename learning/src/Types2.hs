e3bb0e9505005264547dc869516f4058a17243be
snowy615 <yansnow615@gmail.com>
2025-09-18 21:50:41 -0700

    search

--- learning/src/Types2.hs
+++ learning/src/Types2.hs
@@ -7,3 +7,8 @@ import qualified Data.Map as Map
 
 numUniques :: (Eq a) => [a] -> Int
 numUniques = length . nub
+
+search :: (Eq a) => [a] -> [a] -> Bool
+search needle haystack = 
+    let nlen = length needle
+    in foldl (\acc x -> if take nlen x == needle then True else acc) False (tails haystack)
