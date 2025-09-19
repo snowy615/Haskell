4d48bcdd02b24f07748c6280ef900517f8b58e0e
snowy615 <yansnow615@gmail.com>
2025-09-19 16:12:38 -0700

    findKey

--- learning/src/Types2.hs
+++ learning/src/Types2.hs
@@ -22,3 +22,6 @@ encode2 shift msg =
 
 decode2 :: Int -> String -> String
 decode2 shift msg = encode2 (negate shift) msg
+
+findKey :: (Eq k) => k -> [(k, v)] -> Maybe v
+findKey key = foldr (\(k,v) acc -> if key == k then Just v else acc) Nothing
