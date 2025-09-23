7f45163131a082c0aa4c24883ed9727ada83bc0c
snowy615 <yansnow615@gmail.com>
2025-09-22 20:45:51 -0700

    surface

--- learning/src/Geometry.hs
+++ learning/src/Geometry.hs
@@ -19,5 +19,9 @@ cubeVolume a = a ^ 3
 cubeArea :: Float -> Float
 cubeArea a = 6 * a ^ 2
 
+surface ::Shape -> Float
+surface (Circle _ _ r) = pi * r ^2
+surface (Rectangle x1 y1 x2 y2) = 2 * (x1 * y1 + x1 * y2 + x2 * y1)
+
 data Shape = Circle Float Float Float | Rectangle Float Float Float Float
     deriving (Show)
