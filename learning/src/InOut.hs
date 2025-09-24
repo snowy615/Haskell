66e5cf1e79ea7d7afe23044a5d5b01abc8f7ec5f
snowy615 <yansnow615@gmail.com>
2025-09-23 21:58:14 -0700

    return binding to a variable

--- learning/src/InOut.hs
+++ learning/src/InOut.hs
@@ -4,5 +4,6 @@ import Data.Char
 main = do
     putStrLn "Hello, what is your name?"
     name <- getLine
+    feeling <- return "Awesome"
     let capName = map toUpper name
-    putStrLn ("Hey " ++ capName ++ ", you rock!")
+    putStrLn ("Hey " ++ capName ++ ", you rock! " ++ feeling ++ "!")
