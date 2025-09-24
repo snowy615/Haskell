93d4b40b1e1afbe1a1c4a14a9b32f238d1f1a501
snowy615 <yansnow615@gmail.com>
2025-09-23 22:03:30 -0700

    putStr

--- learning/src/InOut.hs
+++ learning/src/InOut.hs
@@ -2,7 +2,8 @@ module InOut where
 
 import Data.Char
 main = do
-    putStrLn "Hello, what is your name?"
+    putStr "Hi! "
+    putStrLn "What is your name?"
     name <- getLine
     feeling <- return "Awesome"
     let capName = map toUpper name
