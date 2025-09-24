7e97cdbfbdd62c4bbf1a920d629d57eb41c7f92a
snowy615 <yansnow615@gmail.com>
2025-09-23 21:51:34 -0700

    let binding

--- learning/src/InOut.hs
+++ learning/src/InOut.hs
@@ -1,6 +1,8 @@
 module InOut where
 
+import Data.Char
 main = do
     putStrLn "Hello, what is your name?"
     name <- getLine
-    putStrLn ("Hey " ++ name ++ ", you rock!")
+    let capName = map toUpper name
+    putStrLn ("Hey " ++ capName ++ ", you rock!")
